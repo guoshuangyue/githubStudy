f1a9dae323e4d26b0678ac2e9df0a69539137e2b
guoshuangyue <18749618325@139.com>
2017-03-29 21:22:18 +0800

    first commit for github

---
+++ javaStudy/src/javaStudy/thingking/java/Case/Page80.java
@@ -0,0 +1,14 @@
+package javaStudy.thingking.java.Case;
+
+public class Page80 {
+
+	public Page80() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
