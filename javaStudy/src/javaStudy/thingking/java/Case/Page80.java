e21b4311566ec1fcb6d7b4d13eff3f1dfb6fe1ae
guoshuangyue <18749618325@139.com>
2017-03-29 21:29:13 +0800

    thinking in java--test case

--- javaStudy/src/javaStudy/thingking/java/Case/Page80.java
+++ javaStudy/src/javaStudy/thingking/java/Case/Page80.java
@@ -8,7 +8,9 @@ public class Page80 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.getProperties().list(System.out);
+		System.out.println(System.getProperty("user.name"));
+		System.out.println(System.getProperty("java.library.path"));
 	}
 
 }
