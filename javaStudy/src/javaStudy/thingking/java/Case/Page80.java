850ecd36de3213002555a19ddce1d8f7b0b1fd9d
guoshuangyue <18749618325@139.com>
2017-04-04 10:26:24 +0800

    test for page89

--- javaStudy/src/javaStudy/thingking/java/Case/Page80.java
+++ javaStudy/src/javaStudy/thingking/java/Case/Page80.java
@@ -2,15 +2,25 @@ package javaStudy.thingking.java.Case;
 
 public class Page80 {
 
+	private static Page80 test;
+	private int i;
+	private char m;
+
 	public Page80() {
 		// TODO Auto-generated constructor stub
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		test = new Page80();
+		test.testForPage89();
 		System.getProperties().list(System.out);
 		System.out.println(System.getProperty("user.name"));
 		System.out.println(System.getProperty("java.library.path"));
 	}
 
+	private void testForPage89() {
+
+		System.out.println(i);
+		System.out.println(m);
+	}
 }
